853d18a9c0766afa8e289015fb9519c935f49812
Eric <1187692525@qq.com>
2016-06-04 00:07:24 +0800

    modify forms.py

--- SystemAction/forms.py
+++ SystemAction/forms.py
@@ -16,5 +16,14 @@ class SaveForm(forms.Form):
     publication_year = forms.CharField(max_length=4,widget=forms.TextInput(attrs={'class': 'form-control'}))
     publication_year.label= u"出版年份"
 
+    add_time = forms.CharField(max_length=4, widget=forms.TextInput(attrs={'class': 'form-control'}))
+    add_time.label = u"上架时间"
+
+    category_id = forms.CharField(max_length=4, widget=forms.TextInput(attrs={'class': 'form-control'}))
+    category_id.label = u"类型"
+
+    counts = forms.CharField(max_length=4, widget=forms.TextInput(attrs={'class': 'form-control'}))
+    counts.label = u"入库数量"
+
     introduction = forms.TextInput(attrs={'class':'form-control'})
 
