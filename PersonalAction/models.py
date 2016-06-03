c14f343cea30821f4127b4ffab1bece585aba182
chaos <497386856@qq.com>
2016-06-03 15:55:46 +0800

    add Persnonal.model: class Book

--- PersonalAction/models.py
+++ PersonalAction/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class Book(models.Model):
+    book_id = models.CharField(max_length=20,primary_key=True,null=False)
+    book_name = models.CharField(max_length=50,null=False)
+    img_url = models.URLField(null=False)
+    author = models.CharField(max_length=50,null=False)
+    press = models.CharField(max_length=50,null=False)
+    publication_year = models.SmallIntegerField(max_length=6,null=False)
+    introduction = models.CharField(max_length=1000)
+    add_time = models.DateField(null=False)
+    price = models.FloatField(decimal_places=2,null=False)
+    category_id = models.CharField(max_length=20,null=False)
+    counts = models.SmallIntegerField(max_length=4,null=False)
