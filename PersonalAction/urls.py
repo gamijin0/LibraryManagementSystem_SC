4efc128e25fe3cbeca623c12cab721d32b955b29
chaos <497386856@qq.com>
2016-06-07 13:20:20 +0800

    borrowbook -completed

--- PersonalAction/urls.py
+++ PersonalAction/urls.py
@@ -3,4 +3,5 @@ from django.conf.urls import patterns, include, url
 urlpatterns = patterns('PersonalAction.views',
     url(r'^status/$', 'status.Status', name='status'),
     url(r'borrow/$','borrowbook.Borrow',name="borrow"),
+    url(r'borrow/(.+)/$','borrowbook.BorrowBook',name="borrowbook"),
 )
--- PersonalAction/views/borrowbook.py
+++ PersonalAction/views/borrowbook.py
@@ -13,49 +13,47 @@ from SystemAction.models import Book
 @login_required()
 @PermissionVerify()
 def Borrow(request):
-    # 提取书籍信息
-    if(request.method=="POST"):
-            # 查询数据库内对象
-        res =ChenkRemain(request.POST)
-        if(res['valid']==True):
-            #修改Book对象
-            oneToSave = Book.objects.get(book_id=request.POST['book_id'])
-            oneToSave.remain_num = oneToSave.remain_num - 1
-            oneToSave.save()
-            #存入Borrow对象
-            import datetime
-            from PersonalAction.models import Borrow
-            from UserManage.models import User
-            oneToBorrow =Borrow(
-                borrow_id=str(oneToSave.book_id)+str(request.user.id)+str(datetime.date.today())
-            )
-            oneToBorrow.user=request.user
-            oneToBorrow.book=oneToSave
-            oneToBorrow.save()
-        # 存储成功后跳转到借书页面
-            return HttpResponseRedirect(reverse('borrow'))
-        else:
-            return HttpResponse(str(res['errors']))
+    # 服务器请求函数
+    booklist = Book.objects.all()
 
-    else:
-        # 服务器请求函数
-        booklist = Book.objects.all()
+    kwvars={
+        'request':request,
+        'booklist':booklist
+    }
 
-        kwvars={
-            'request':request,
-            'booklist':booklist
-        }
-
-        return render_to_response('PersonalAction/borrow.html',kwvars,RequestContext(request))
+    return render_to_response('PersonalAction/borrow.html',kwvars,RequestContext(request))
 
 
-def ChenkRemain(POST):
+def ChenkRemain(book_id):
 
     valid = True
     erros = list()
 
-    if(POST['remain_num']<=0):
+    if(Book.objects.get(book_id=book_id).remain_num<=0):
         erros.append("There is no book to lend!")
         valid=False
 
     return {'valid':valid,'errors':erros}
+
+def BorrowBook(request,book_id):
+    # 查询数据库内对象
+    res = ChenkRemain(book_id)
+    if (res['valid'] == True):
+        # 修改Book对象
+        oneToSave = Book.objects.get(book_id=book_id)
+        oneToSave.remain_num = oneToSave.remain_num - 1
+        oneToSave.save()
+        # 存入Borrow对象
+        import datetime
+        from PersonalAction.models import Borrow
+        from UserManage.models import User
+        oneToBorrow = Borrow(
+            borrow_id=str(oneToSave.book_id) + str(request.user.id) + str(datetime.date.today())
+        )
+        oneToBorrow.user = request.user
+        oneToBorrow.book = oneToSave
+        oneToBorrow.save()
+        # 存储成功后跳转到借书页面
+        return HttpResponseRedirect(reverse('borrow'))
+    else:
+        return HttpResponse(str(res['errors']))
